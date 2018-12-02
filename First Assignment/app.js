6f7cfaa0502000eeb9fc9367287910d10e9c7953
MatthewDodi <matthew.dodi@gmail.com>
2018-12-02 15:07:40 -0800

    type: feat installed nodemon and express.js

--- First Assignment/app.js
+++ First Assignment/app.js
@@ -2,29 +2,9 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   const url = req.url;
-  const body = [];
-  const method = req.method;
   if (url === '/') {
-    res.write('<h1>Hello</h1>');
-    return res.end();
-  }
-  if (url === '/users') {
-    res.write('<html>');
-    res.write('<head><title>Enter Message</title></head>');
-    res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message" /><button type="submit">Send</button></form></body>'
-    );
-    res.write('</html>');
-    return res.end();
-  }
-  if (url === '/sent' && method === 'POST') {
-    req.on('data', chunk => {
-      body.push(chunk);
-    });
-    req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString();
-      console.log(parsedBody);
-    });
+    res.end('Bye');
   }
 });
-server.listen(3000, () => console.log('App running on port 3000'));
+
+server.listen(3000);
