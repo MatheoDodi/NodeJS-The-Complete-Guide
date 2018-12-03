de91fb1a3537b1da1ba92673a74fc91452c0ddcd
MatthewDodi <matthew.dodi@gmail.com>
2018-12-03 01:00:04 -0800

    added body-parser middleware

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const bodyparser = require('body-parser');
 
 const app = express();
 
+app.use(bodyparser.urlencoded({ extended: false }));
+
 app.use('/add-product', (req, res, next) => {
-  res.send('<h1>The Add Product Page</h1>');
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button>Add Product</button></form>'
+  );
+});
+
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
