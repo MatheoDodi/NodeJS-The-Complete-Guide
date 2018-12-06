a60bc4006d6dab40cf4ffd1d73f80c39b78b5464
MatthewDodi <matthew.dodi@gmail.com>
2018-12-05 20:27:12 -0800

    type: feat - Added EJS templating language

--- app.js
+++ app.js
@@ -8,15 +8,17 @@ const app = express();
 const adminData = require('./routes/admin');
 const shopRouter = require('./routes/shop');
 
-app.engine(
-  'handlebars',
-  expressHbs({
-    layoutsDir: path.join(__dirname, '/views/layouts'),
-    defaultLayout: 'main-layout',
-    extname: 'handlebars'
-  })
-);
-app.set('view engine', 'handlebars'); // Setting the templating Engine
+// Set up for express-handlebars
+// app.engine(
+//   'handlebars',
+//   expressHbs({
+//     layoutsDir: path.join(__dirname, '/views/layouts'),
+//     defaultLayout: 'main-layout',
+//     extname: 'handlebars'
+//   })
+// );
+
+app.set('view engine', 'ejs'); // Setting the templating Engine
 app.set('views', 'views'); // Setting the folder from which to load views from
 
 app.use(bodyparser.urlencoded({ extended: false }));
